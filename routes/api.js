dace8c73077740fb1a3e52d3a978936546a6ea80
- <74578072+darmawan06@users.noreply.github.com>
2022-12-09 22:29:59 +0700

    Update

--- routes/api.js
+++ routes/api.js
@@ -26,6 +26,31 @@ router.get('/:req_title', (req, res, next)=> {
   res.send(data[req.params.req_title]);
 });
 
+router.post('/produk/add',multerSingle({req_name : "image", location_path : "./public/images/products/"}), (req,res,next)=>{
+  try{
+    var uniq = Date.now().toString(36) + Math.floor(Math.pow(10, 12) + Math.random() * 9*Math.pow(10, 12)).toString(36);
+      data.produk.push({
+        id : uniq,
+        name : req.body.name || "",
+        desc : req.body.deskripsi || "",
+        specs : req.body.specs.split(',') || [""],
+        jenis : req.body.jenis.split(',') || [""],
+        stok : req.body.stok || "",
+        img : (req.file) ? `${configs.base_url}/images/products/${req.file.filename}` : ""
+      })  
+      res.send({
+        error : "200",
+        message : `Success Tambah Portofolio`
+      });
+  }catch(e){
+    res.send({
+      error : "404",
+      message : `Failed Tambah Portofolio`
+    });
+  }
+})
+
+
 router.get('/produk/:id', (req,res,next)=>{
   const filterDataProduct = data.produk.filter((e)=>{
     return e.id == req.params.id
@@ -40,12 +65,137 @@ router.get('/produk/:id', (req,res,next)=>{
   }
 })
 
+router.post('/produk',multerSingle({req_name : "image", location_path : "./public/images/products/"}) ,(req, res, next)=> {
+  try{
+    data.produk.find((e)=>{
+        if(e.id == req.body.id){
+          e.name = req.body.name,
+          e.desc = req.body.deskripsi,
+          e.specs = req.body.specs.split(',') || [""],
+          e.jenis = req.body.jenis.split(',') || [""],
+          e.stok = req.body.stok,
+          e.img = (req.file) ? `${configs.base_url}/images/products/${req.file.filename}` : e.img
+          res.send({
+            error : "200",
+            message : `Success Update Product`
+          });
+        }
+    });
+  }catch(e){
+    res.send({
+      error : "404",
+      message : `Failed Update Product`
+    });
+  }
+});
+
+router.post('/produk/delete', (req,res,next)=>{
+  try{
+    const objWithIdIndex = data.produk.findIndex((obj) => obj.id == req.body.id);
+
+    if (objWithIdIndex > -1) {
+      data.produk.splice(objWithIdIndex, 1);
+      res.send({
+        error : "200",
+        message : `Success Delete Product`
+      });   
+    }
+  }catch(e){
+    res.send({
+      error : "404",
+      message : `Failed Delete Product`
+    });
+  }
+})
+
+router.get('/portofolio/:id', (req,res,next)=>{
+  const filterData = data.portofolio.filter((e)=>{
+    return e.id == req.params.id
+  })
+  if(filterData.length != 0){
+    res.send(filterData)
+  }else{
+    res.send({
+      error : "505",
+      message : `We No Have Product By ID ${req.params.id}`
+    });
+  }
+})
+
+router.post('/portofolio/delete', (req,res,next)=>{
+  try{
+    const objWithIdIndex = data.portofolio.findIndex((obj) => obj.id == req.body.id);
+
+    if (objWithIdIndex > -1) {
+      data.portofolio.splice(objWithIdIndex, 1);
+      res.send({
+        error : "200",
+        message : `Success Delete Portofolio`
+      });   
+    }
+  }catch(e){
+    res.send({
+      error : "404",
+      message : `Failed Delete Portofolio`
+    });
+  }
+})
+
+router.post('/portofolio/add',multerSingle({req_name : "image", location_path : "./public/images/portofolio/"}), (req,res,next)=>{
+  try{
+    var uniq = Date.now().toString(36) + Math.floor(Math.pow(10, 12) + Math.random() * 9*Math.pow(10, 12)).toString(36);
+    if(req.body != null && req.file){
+      data.portofolio.push({
+        id : uniq,
+        img : `${configs.base_url}/images/portofolio/${req.file.filename}` || "",
+        deskripsi : req.body.deskripsi || ""
+      })  
+      res.send({
+        error : "200",
+        message : `Success Delete Portofolio`
+      });
+    }else{
+      throw error;
+    }
+  }catch(e){
+    res.send({
+      error : "404",
+      message : `Failed Delete Portofolio`
+    });
+  }
+})
+
+
+router.post('/portofolio',multerSingle({req_name : "image", location_path : "./public/images/portofolio/"}) ,(req, res, next)=> {
+  try{
+      data.portofolio.find((e)=>{
+        if(e.id == req.body.id){
+          if(req.file){
+            e.img = `${configs.base_url}/images/portofolio/${req.file.filename}`
+          }
+          e.deskripsi = req.body.deskripsi 
+        }
+      })
+      res.send({
+        error : "200",
+        message : `Success Updated Portofolio`
+      });
+  }catch(e){
+      res.send({
+        error : "404",
+        message : `Failed Updated Portofolio`
+      });
+  }
+});
+
 
 router.post('/testimoni',(req, res, next)=> {
     var uniq = Date.now().toString(36) + Math.floor(Math.pow(10, 12) + Math.random() * 9*Math.pow(10, 12)).toString(36);
     if(req.body.name != null){
       data.testimoni.push({
         id : uniq,
+        date: formattedToday,
+        email : req.body.email || "",
         name : req.body.name || "",
         rating : parseInt(req.body.rating) || "",
         deskripsi : req.body.deskripsi || ""
@@ -83,7 +233,7 @@ router.post('/visitors' ,(req, res, next)=> {
         city : req.body.city || "Not Found",
         country : req.body.country || "Not Found",
         isp : req.body.isp || "Not Found",
-        regionName : req.body.regionName || "Not Found",
+        region : req.body.regionName || "Not Found",
       })    
       res.send({
         error : "201",
--- routes/data.js
+++ routes/data.js
@@ -80,47 +80,48 @@ let data = JSON.parse(`{
     "img": "${configs.base_url}/images/portofolio/(6).jpeg",
     "deskripsi": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer"
   }, {
-    "id": 5005,
+    "id": 5006,
     "img": "${configs.base_url}/images/portofolio/(7).jpeg",
     "deskripsi": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer"
   }, {
-    "id": 5005,
+    "id": 5007,
     "img": "${configs.base_url}/images/portofolio/(8).jpeg",
     "deskripsi": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer"
   }, {
-    "id": 5005,
+    "id": 5008,
     "img": "${configs.base_url}/images/portofolio/(9).jpeg",
     "deskripsi": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer"
   }, {
-    "id": 5005,
+    "id": 5009,
     "img": "${configs.base_url}/images/portofolio/(10).jpeg",
     "deskripsi": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer"
   }, {
-    "id": 5005,
+    "id": 5010,
     "img": "${configs.base_url}/images/portofolio/(11).jpeg",
     "deskripsi": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer"
   }],
   "produk": [{
     "id": 5001,
     "name": "Barrier",
-    "img": ["${configs.base_url}/images/products/Barier F 500x1000x1000.jpg", "${configs.base_url}/images/products/Barier M 500x1000x1000.jpg"],
+    "img": "${configs.base_url}/images/products/Barier F 500x1000x1000.jpg",
     "desc": "Barrier adalah produk precast yang digunakan sebagai pembatas jalan",
     "specs": ["Mutu beton K-350", "Mutu besi U-50 Hard Drawn Deformed Wire (HDDW)",
       "Cetak Basah dengan getaran frekuensi tinggi",
       "System join male-female"
     ],
-    "dimensi": ["Lebar : 60cm dan 80cm", "Panjang : 100cm dan 120cm", "Tinggi : 70cm s.d 120cm"],
-    "jenis": null
+    "stok" : 80,
+    "jenis": ["Lebar : 60cm dan 80cm", "Panjang : 100cm dan 120cm", "Tinggi : 70cm s.d 120cm"]
   }, {
     "id": 5002,
     "name": "U-DITCH",
-    "img": ["${configs.base_url}/images/products/3d Uditch 40x40 F1.jpg", "${configs.base_url}/images/products/3d Uditch 40x40 F2.jpg", "${configs.base_url}/images/products/3d Uditch 40x40 M2.jpg", "${configs.base_url}/images/products/3d Uditch 40x40 M1.jpg"],
+    "img": "${configs.base_url}/images/products/3d Uditch 40x40 F1.jpg",
     "desc": "Uditch adalah beton bertulang dengan bentuk penampang huruf U yang berfungsi sebagai saluran Drainase. Uditch memiliki daya lateral yang tinggi, mampu menahan beban dari tanah dan juga alirai air.",
     "specs": ["Mutu beton K-350", "Mutu besi U-50 Hard Drawn Deformed Wire (HDDW)",
       "Cetak Basah dengan getaran frekuensi tinggi",
       "System join male-female"
     ],
-    "dimensi": ["30 x 30 x 120 Cm",
+    "stok" : 80,
+    "jenis": ["30 x 30 x 120 Cm",
       "30 x 40 x 120 Cm",
       "30 x 50 x 120 Cm",
       "40 x 40 x 120 Cm",
@@ -136,59 +137,58 @@ let data = JSON.parse(`{
       "80 x 100 x 120 Cm",
       "100 x 100 x 120 Cm",
       "100 x 120 x 120 Cm"
-    ],
-    "jenis": null
+    ]
   }, {
     "id": 5003,
     "name": "COVER U-DITCH",
-    "img": ["${configs.base_url}/images/products/cover 40 1.jpg", "${configs.base_url}/images/products/cover 40 2.jpg", "${configs.base_url}/images/products/cover 40 3.jpg"],
+    "img": "${configs.base_url}/images/products/cover 40 1.jpg",
     "desc": "Cover U-ditch adalah merupakan tutup saluran air drainase beton pracetak yang berbentuk huruf U atau U-ditch.",
     "specs": ["Mutu beton K-350", "Mutu besi U-50 Hard Drawn Deformed Wire (HDDW)",
       "Cetak Basah dengan getaran frekuensi tinggi",
       "System join male-female"
     ],
-    "dimensi": ["30 x 60 x 9 Cm",
+    "stok" : 80,
+    "jenis": ["30 x 60 x 9 Cm",
       "40 x 60 x 10 Cm",
       "50 x 60 x 12 Cm",
       "60 x 60 x 12 Cm",
       "80 x 60 x 15 Cm"
-    ],
-    "jenis": null
+    ]
   }, {
     "id": 5004,
     "name": "BOX CULVERT",
-    "img": ["${configs.base_url}/images/products/BC 100x100x100 M1.jpg", "${configs.base_url}/images/products/BC 100x100x100 M2.jpg", "${configs.base_url}/images/products/BC 100x100x100.jpg", "${configs.base_url}/images/products/BC 60x60x100 F1.jpg", "${configs.base_url}/images/products/BC 60x60x100 F2.jpg", "${configs.base_url}/images/products/BC 60x60x100 M1.jpg", "${configs.base_url}/images/products/BC 60x60x100 M2.jpg"],
+    "img": "${configs.base_url}/images/products/BC 100x100x100 M1.jpg",
     "desc": "Box Culvert adalah merupakan material beton yang banyak digunakan untuk keperluan saluran air sehingga ia banyak dikenal masyarakat dengan sebutan gorong-gorong. Betuk Box Culvert sendiri kotak atau persegi sesuai dengan nama yang diberikan. Box Culvert dapat diaplikasikan pada area kontruksi bawah tanah baik untuk gorong-gorong jembatan, terowongan, gorong-gorong kereta api dan lain sebagainya.",
     "specs": ["Mutu beton K-350", "Mutu besi U-50 Hard Drawn Deformed Wire (HDDW)",
       "Cetak Basah dengan getaran frekuensi tinggi",
       "System join male-female"
     ],
-    "dimensi": ["40 x 40 x 100 Cm",
+    "stok" : 80,
+    "jenis": ["40 x 40 x 100 Cm",
       "50 x 50 x 100 Cm",
       "60 x 60 x 100 Cm",
       "80 x 80 x 100 Cm",
       "100 x 100 x 100 Cm"
-    ],
-    "jenis": null
+    ]
   }, {
     "id": 5005,
     "name": "CANSTEEN",
-    "img": ["${configs.base_url}/images/products/kansteen 18x32x60.jpg", "${configs.base_url}/images/products/kansteen M 18x32x60.jpg", "${configs.base_url}/images/products/kansteen F 22x25x60.jpg", "${configs.base_url}/images/products/kansteen m 22x25x60.jpg"],
+    "img": "${configs.base_url}/images/products/kansteen 18x32x60.jpg",
     "desc": "produk beton precast yang banyak dijumpai di pinggir badan jalan. Fungsi utamanya adalah sebagai pembatas antara badan jalan dan trotoar.",
     "specs": ["Mutu Beton K-350",
       "Tidak Bertulang",
       "Model Cansteen dapat disesuaikan dengan permintaan",
       "Join male-female"
     ],
-    "dimensi": null,
+    "stok" : 80,
     "jenis": null
   }, {
     "id": 5006,
     "name": "PAVING BLOCK",
-    "img": ["${configs.base_url}/images/products/paving block.jpg"],
+    "img": "${configs.base_url}/images/products/paving block.jpg",
     "desc": "Paving Block atau conblock dan adapula yang menyebutnya bata block, merupakan produk beton berbentuk block kecil dan disusun diatas tanah dengan tujuan membuat jalan lebih padat, keras dan rata.",
     "specs": ["Mutu Beton K-250 s.d K-400"],
-    "dimensi": null,
+    "stok" : 80,
     "jenis": ["Model Bata",
       "Trihex",
       "Cacing",
@@ -201,27 +201,37 @@ let data = JSON.parse(`{
   }],
   "testimoni": [{
     "id": 5001,
+    "date" : "2022-12-07",
     "name": "Asep Balon",
+    "email" : "xxxxx@gmail.com",
     "rating": 2,
     "deskripsi": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer"
   }, {
     "id": 5002,
+    "date" : "2022-12-07",
     "name": "Asep Kurnia",
+    "email" : "xxxxx@gmail.com",
     "rating": 5,
     "deskripsi": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer"
   }, {
     "id": 5003,
+    "date" : "2022-12-07",
     "name": "Asep Saepul",
+    "email" : "xxxxx@gmail.com",
     "rating": 3,
     "deskripsi": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer"
   }, {
     "id": 5004,
+    "date" : "2022-12-07",
     "name": "Ridho Roma",
+    "email" : "xxxxx@gmail.com",
     "rating": 1,
     "deskripsi": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer"
   }, {
     "id": 5005,
+    "date" : "2022-12-07",
     "name": "Ridho Ajah",
+    "email" : "xxxxx@gmail.com",
     "rating": 2,
     "deskripsi": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer"
   }],
